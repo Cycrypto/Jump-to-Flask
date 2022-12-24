84d5d0e37ded584ae925b0c4426c0ef2a33680b7
Cycrypto <jh01love@naver.com>
2022-12-24 10:49:54 +0900

    Add Question, Answer models

---
+++ projects/pybo/models.py
@@ -0,0 +1,19 @@
+from pybo import db
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateETime(), nullable=False)
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
+    question = db.relationship('Question', backref=db.backref('anser_set'))
+    # backref : 역참조 옵션 :: 답변모델에서 연결된 질문 모델의 제목을 answer.question.subject처럼 참조할 수 있다
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
+
+
+
